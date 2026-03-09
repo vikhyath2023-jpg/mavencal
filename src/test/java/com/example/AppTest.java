f9689313e4ed78e4151891c2b51ba9e3db1dae93
vikhyath2023-jpg <vikhyath.veluri2023@vitstudent.ac.in>
2026-03-09 21:41:15 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,25 @@
 package com.example;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-public class AppTest {
-    @Test
-    public void testGradeA() {
-        App app = new App();
-        assertEquals("A", app.getGrade(85));
-    }
-}
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+    App calc = new App();
+
+    @Test
+    public void testAddition(){
+        assertEquals(10, calc.add(5,5));
+    }
+
+    @Test
+    public void testSubtraction(){
+        assertEquals(2, calc.subtract(5,3));
+    }
+
+    @Test
+    public void testMultiplication(){
+        assertEquals(15, calc.multiply(5,3));
+    }
+
+}
